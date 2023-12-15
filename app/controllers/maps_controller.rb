1e00dfb7e603a444399aaa160f7e1c874c3ebda5
Tony Masek <tm@tony-masek.com>
2023-12-15 10:13:25 +0000

    Create view and display information

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,7 +1,7 @@
 class MapsController < ApplicationController
 
   def index
-    # mechanize = Mechanize.new
+    @virtual_browser = Mechanize.new
     @sanderus_maps = map_scrapping_sanderus
   end
 
@@ -9,10 +9,15 @@ class MapsController < ApplicationController
 
   # Builds out a hash for each map to be displayed on the idex from the sanderusmaps.com web page
   def map_scrapping_sanderus
+    # Uncomment once in production
     # url = "https://sanderusmaps.com/search?q_cat=&q_title=&q_keywords=&q_mapmaker=#{map_maker}&q_mapnum=&_gl=1*5yozj9*_up*MQ..*_ga*MTk4OTIxODY3OS4xNzAyNTg3MDU2*_ga_4GV6JSEDD8*MTcwMjU4NzA1NS4xLjEuMTcwMjU4NzMyNC4wLjAuMA.."
-    # page = mechanize.get(url)
+    # page = @virtual_browser.get(url)
+    # document = Nokogiri::HTML(page.body)
+
+    # Local downloaded page scrapping (for dev purposes)
     html_content = File.read('app/assets/pages_to_scrape/map.html')
     document = Nokogiri::HTML(html_content)
+
     document.css('.proditem').map do |map|
       {
         map_show_page_link: map['href'],
