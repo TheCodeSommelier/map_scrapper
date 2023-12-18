4ee1dae8b62984606af41d553d108d4a7883b9f8
Tony Masek <tm@tony-masek.com>
2023-12-18 21:44:09 +0100

    Add a gem create logic on crawling pages

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,28 +1,42 @@
 class MapsController < ApplicationController
-
   def index
+    map_maker = "Sebastian Münster"
     @virtual_browser = Mechanize.new
-    sanderus_maps = map_scrapping_sanderus
-    # lazarova_maps = map_scrapping_lazarova
-    # p raremaps_maps = map_scrapping_raremaps
-    # @all_scrapped_maps = sanderus_maps + raremaps_maps + lazarova_maps
-    @all_scrapped_maps = sanderus_maps
+    s_maps = map_scrapping_s(map_maker)
+    r_maps = map_scrapping_r(map_maker)
+    l_maps = map_scrapping_l(map_maker)
+    @all_scrapped_maps = r_maps + s_maps + l_maps
   end
 
   private
 
+  # !!! Antique e-shop "S" !!!
   # Builds out a hash for each map to be displayed on the idex from the sanderusmaps.com web page
-  def map_scrapping_sanderus
-    # Uncomment once in production
-    # url = "https://sanderusmaps.com/search?q_cat=&q_title=&q_keywords=&q_mapmaker=#{map_maker}&q_mapnum=&_gl=1*5yozj9*_up*MQ..*_ga*MTk4OTIxODY3OS4xNzAyNTg3MDU2*_ga_4GV6JSEDD8*MTcwMjU4NzA1NS4xLjEuMTcwMjU4NzMyNC4wLjAuMA.."
-    # page = @virtual_browser.get("https://sanderusmaps.com/search?q_cat=&q_title=&q_keywords=&q_mapmaker=Sebastian+M%C3%BCnster&q_mapnum=&_gl=1*1v82r1x*_up*MQ..*_ga*Mjk5NTQ4ODYuMTcwMjY0MzUzNg..*_ga_4GV6JSEDD8*MTcwMjY0MzUzNS4xLjEuMTcwMjY0MzUzOS4wLjAuMA..")
-    # document = Nokogiri::HTML(page.body)
+  def map_scrapping_s(map_maker)
+    array_of_maps = []
+    s_page = @virtual_browser.get("#{ENV.fetch('BASE_URL_S')}#{URI.encode_www_form_component(map_maker)}",
+                                  { headers: { "User-Agent" => user_agent_picker } })
+    pages_urls = pagification_sanderus(Nokogiri::HTML(s_page.body))
+    pages_urls.each do |page_url|
+      maps_index_page_html = @virtual_browser.get(page_url,
+                                                  { headers: { "User-Agent" => user_agent_picker } })
+      array_of_maps += s_map_hash_builder(Nokogiri::HTML(maps_index_page_html.body))
+    end
+    array_of_maps
+  end
 
-    # Local downloaded page scrapping (for dev purposes)
-    html_content = File.read('app/assets/pages_to_scrape/map.html')
-    document = Nokogiri::HTML(html_content)
+  # Gets pages from  the Sanderus website
+  def pagification_sanderus(html_document)
+    array_of_pages = []
+    html_document.css("li a").each do |list_item|
+      array_of_pages << list_item['href'] if list_item.text.match(/\d{1}/) && list_item.attr("class") != "cart"
+    end
+    array_of_pages.uniq
+  end
 
-    document.css('.proditem').map do |map|
+  # Builds the hash for the map from Sanderus page
+  def s_map_hash_builder(html_document)
+    html_document.css('.proditem').map do |map|
       {
         map_show_page_link: map['href'],
         map_image_url: map.css('.img').children[1].children[1].values[-1],
@@ -32,15 +46,17 @@ class MapsController < ApplicationController
     end
   end
 
-  def map_scrapping_raremaps
-    # Uncomment once in production
-    # url = "https://www.raremaps.com/inventory/search?q=#{map_maker}"
-    page = @virtual_browser.get("https://www.raremaps.com/inventory/search?q=Sebastien+munster")
-    document = Nokogiri::HTML(page.body)
+  # !!! Antique e-shop "R" !!!
+  def map_scrapping_r(map_maker)
+    r_page = @virtual_browser.get("#{ENV.fetch('BASE_URL_R')}#{URI.encode_www_form_component(map_maker)}",
+                                  { headers: { "User-Agent" => user_agent_picker } })
+    r_map_hash_builder(Nokogiri::HTML(r_page.body))
+  end
 
-    document.css('.item.card').map do |map|
+  def r_map_hash_builder(html_document)
+    html_document.css('.item.card').map do |map|
       {
-        map_show_page_link: "https://www.raremaps.com#{map.css('.image').children[1]['href']}",
+        map_show_page_link: "#{ENV.fetch('BASE_URL_R_MAP_SHOW_PAGE')}#{map.css('.image').children[1]['href']}",
         map_image_url: map.css('.image').children[1].children[1]['src'],
         map_title: map.css('.info').children[1].css('.title').text.strip,
         map_price: map.css('aside').children[1].text.strip.tr(" ", "")
@@ -48,19 +64,27 @@ class MapsController < ApplicationController
     end
   end
 
-  def map_scrapping_lazarova
-    # Uncomment once in production
-    # url = "https://www.raremaps.com/inventory/search?q=#{map_maker}"
-    page = @virtual_browser.get("https://www.antikvariat-marketa-lazarova.cz/510446575/e-search?q=Sebastian+M%C3%BCnster")
-    document = Nokogiri::HTML(page.body)
+  # !!! antique e-shop "L" !!!
+  def map_scrapping_l(map_maker)
+    l_page = @virtual_browser.get("#{ENV.fetch('BASE_URL_L')}#{URI.encode_www_form_component(map_maker)}",
+                                  { headers: { "User-Agent" => user_agent_picker } })
+    l_map_hash_builder(Nokogiri::HTML(l_page.body))
+  end
 
-    document.css('.product').map do |map|
+  def l_map_hash_builder(html_document)
+    html_document.css('.product').map do |map|
       {
-        map_show_page_link: "https://www.antikvariat-marketa-lazarova.cz#{map.css('.c309 a').attr('href').value}",
-        map_image_url: "https://www.antikvariat-marketa-lazarova.cz#{map.css('.c309 a img').attr('src').value}",
+        map_show_page_link: "#{ENV.fetch('BASE_URL_L_MAP_SHOW_PAGE_AND_PIC')}#{map.css('.c309 a').attr('href').value}",
+        map_image_url: "#{ENV.fetch('BASE_URL_L_MAP_SHOW_PAGE_AND_PIC')}#{map.css('.c309 a img').attr('src').value}",
         map_title: map.css('.c309 a').attr('title').value,
         map_price: "KČ#{map.attr('data-price')}"
       }
     end
+  end
+
+  # This method picks a random user agent to use in the scraping methods
+  def user_agent_picker
+    user_agents = File.readlines("user_agents.txt", chomp: true)
+    user_agents.sample
   end
 end
