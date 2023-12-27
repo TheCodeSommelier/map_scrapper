f8d25f195d9d300648bec960b8a3df8f9cb93de8
Tony Masek <tm@tony-masek.com>
2023-12-27 18:36:47 +0100

    Create method to randomise time of scraping

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,11 @@
-Sidekiq.configure_server do |config|
-  config.on(:startup) do
-    schedule_file = "config/schedule.yml"
+# Sidekiq.configure_server do |config|
+#   config.on(:startup) do
+#     schedule_file = "config/schedule.yml"
 
-    if File.exist?(schedule_file)
-      schedule = YAML.load_file(schedule_file)
+#     if File.exist?(schedule_file)
+#       schedule = YAML.load_file(schedule_file)
 
-      Sidekiq::Cron::Job.load_from_hash!(schedule, source: "schedule")
-    end
-  end
-end
+#       Sidekiq::Cron::Job.load_from_hash!(schedule, source: "schedule")
+#     end
+#   end
+# end
