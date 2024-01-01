53e1f97c1f001d9a643d4841a9fdf991a5e71c0f
Tony Masek <tm@tony-masek.com>
2024-01-01 19:29:58 +0100

    Went back to the old saving logic

--- app/jobs/schedule_job.rb
+++ app/jobs/schedule_job.rb
@@ -2,7 +2,7 @@ class ScheduleJob < ApplicationJob
   queue_as :scheduler
 
   def perform
-    minute = rand(5..10)
+    minute = rand(1..4)
     next_run_time = Time.now + minute.minutes
     ScraperWorker.perform_at(next_run_time)
   end
--- app/jobs/scrape_r_job.rb
+++ app/jobs/scrape_r_job.rb
@@ -33,21 +33,20 @@ class ScrapeRJob < ApplicationJob
                                   { headers: { "User-Agent" => @user_agent } })
       r_map_instance_builder(Nokogiri::HTML(page.body), map_maker)
     end
-
+    array_of_maps.compact!
     Map.import(@map_columns, array_of_maps, batch_size: 20)
   end
 
   # Builds instances of maps from Antique e-shop "R" with attributes of antique maps
   def r_map_instance_builder(r_html_document, map_maker)
     r_html_document.css('.item.card').map do |map|
-      map_attrs = {
+      Map.new(
         title: map.css('.info').children[1].css('.title').text.strip,
         price: map.css('aside').children[1].text.strip.tr(" ", ""),
         map_show_page_link: "#{ENV.fetch('BASE_URL_R_MAP_SHOW_PAGE')}#{map.css('.image').children[1]['href']}",
         image_url: map.css('.image').children[1].children[1]['src'],
         map_maker: map_maker
-      }
-      Map.where(map_attrs).empty? ? Map.new(map_attrs) : next
+      )
     end
   end
 
