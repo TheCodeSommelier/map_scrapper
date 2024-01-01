384fb72cfea573f6753da931b82d6ea27c78743a
Tony Masek <tm@tony-masek.com>
2024-01-01 20:02:05 +0100

    Prolonged the intervals to be one week

--- app/jobs/schedule_job.rb
+++ app/jobs/schedule_job.rb
@@ -2,8 +2,23 @@ class ScheduleJob < ApplicationJob
   queue_as :scheduler
 
   def perform
-    minute = rand(1..4)
-    next_run_time = Time.now + minute.minutes
+    next_week = 1.week.from_now
+    start_of_nw = next_week.beginning_of_week
+    seconds_in_nw = (start_of_nw + 1.week) - start_of_nw
+    next_run_time = start_of_nw + rand(seconds_in_nw)
+
+    puts "This is next_week"
+    p next_week
+
+    puts "This is start_of_nw"
+    p start_of_nw
+
+    puts "This is seconds_in_nw"
+    p seconds_in_nw
+
+    puts "This is next_run_time"
+    p next_run_time
+
     ScraperWorker.perform_at(next_run_time)
   end
 end
